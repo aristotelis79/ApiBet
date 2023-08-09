1e46ab2e1d12a56717dc41187238e0e2d1fb3663
aristotelis <aristotelis79@gmail.com>
2023-08-09 18:55:59 +0300

    small refactor

--- repository/league_repository.py
+++ repository/league_repository.py
@@ -2,6 +2,7 @@ import os
 import constants
 import csv
 import json
+import encodings
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -16,24 +17,6 @@ class LeagueRepository:
         self._available_leagues_filepath = constants.AVAILABLE_LEAGUES_FILEPATH
         self._saved_leagues_directory = constants.SAVED_LEAGUES_DIRECTORY
         os.makedirs(self._saved_leagues_directory, exist_ok=True)
-
-    def _league_table_path(self, country: str, divivsion: str) -> str:
-        return self._saved_leagues_directory + constants.LEAGUE_TABLE.format(country,divivsion)
-
-    def _league_config_path(self, country: str, divivsion: str) -> str:
-        return self._saved_leagues_directory + constants.LEAGUE_CONFIG.format(country,divivsion)
-
-    def get_league_config(self, country: str, division: str) -> LeagueConfig or None:
-        with open(self._league_config_path(country, division), 'r', encoding='utf-8') as fp:
-            league_config: LeagueConfig = json.load(fp) 
-        return league_config
-
-    def save_league_config(self, league_config: LeagueConfig) -> None:
-        with open(self._league_config_path(league_config.country, league_config.name), 'r', encoding='utf-8') as fp:
-            json.dump(league_config, fp)
-
-    def league_exists(self, country: str, divivsion: str) -> bool:
-        return os.path.exists(self._league_table_path(country,divivsion))
 
     def get_league(self, country: str, division: str) -> League or None:
          return self._get_all_available_leagues()[(country,division)]
@@ -50,10 +33,9 @@ class LeagueRepository:
             league_config)
     
     def load_league(self, league: League) -> Union[pd.DataFrame, None]:
-        if self.league_exists(league.country, league.name):
+        if self._league_exists(league.country, league.name):
             league_table = pq.read_table(self._league_table_path(league.country, league.name))
             return league_table.to_pandas()
-        
         return None
 
     def save_league(self, 
@@ -62,7 +44,7 @@ class LeagueRepository:
                     league_config: LeagueConfig, 
                     update_on_exist: bool = True) -> None:
         
-        if self.league_exists(league.country, league.name) and update_on_exist is False:
+        if self._league_exists(league.country, league.name) and update_on_exist is False:
             return None
         
         matches_df = StatisticsEngine(
@@ -74,18 +56,28 @@ class LeagueRepository:
         table = pa.Table.from_pandas(matches_df)
         pa.parquet.write_table(table, self._league_table_path(league.country, league.name))
 
-        self.save_league_config(league_config)
+        self._save_league_config(league_config)
 
     def delete_league(self, league: League) -> bool:
-        if self.league_exists(league.country, league.name) is False:
+        if self._league_exists(league.country, league.name) is False:
             return False
         
         os.remove(self._league_table_path(league.country, league.name))
         os.remove(self._league_config_path(league.country, league.name))
         return True
 
+    def get_league_config(self, country: str, division: str) -> LeagueConfig or None:
+        with open(self._league_config_path(country, division), 'r', encoding=encodings.utf_8.getregentry().name) as fp:
+            league_config: LeagueConfig = json.load(fp) 
+        return league_config
+
+    def to_csv(self, df: pd.DataFrame, league: League) -> None:
+        league_filepath = f'{self._saved_leagues_directory}{league.country}{league.name}.csv'
+        df.to_csv(league_filepath, index=False)
+
     def _get_all_available_leagues(self) -> dict:
-        with open(file=constants.AVAILABLE_LEAGUES_FILEPATH, mode='r', encoding='utf=8') as csvfile:
+        with open(file=constants.AVAILABLE_LEAGUES_FILEPATH, 
+                  mode='r', encoding= encodings.utf_8.getregentry().name) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             next(reader)
             return {(row[0], row[1]): League(
@@ -95,7 +87,17 @@ class LeagueRepository:
                 year_start=int(row[3]),
                 league_type=row[4],
                 upcoming_fixtures_url=row[5]) for row in reader}
+        
+    def _league_exists(self, country: str, divivsion: str) -> bool:
+        return os.path.exists(self._league_table_path(country,divivsion))
+    
+    def _league_table_path(self, country: str, divivsion: str) -> str:
+        return self._saved_leagues_directory + constants.LEAGUE_TABLE.format(country,divivsion)
 
-    def to_csv(self, df: pd.DataFrame, league: League) -> None:
-        league_filepath = f'{self._saved_leagues_directory}{league.country}{league.name}.csv'
-        df.to_csv(league_filepath, index=False)
+    def _league_config_path(self, country: str, divivsion: str) -> str:
+        return self._saved_leagues_directory + constants.LEAGUE_CONFIG.format(country,divivsion)
+
+    def _save_league_config(self, league_config: LeagueConfig) -> None:
+        with open(self._league_config_path(league_config.country, league_config.name),
+                   'r', encoding=encodings.utf_8.getregentry().name) as fp:
+            json.dump(league_config, fp)
